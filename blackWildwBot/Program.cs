52735d8cb3b962b4801dfe080e9b8cca6e14e93a
Oh-Shit-God-Damn <workthebest666@gmail.com>
2019-11-28 22:08:39 +0200

    commit

--- blackWildwBot/Program.cs
+++ blackWildwBot/Program.cs
@@ -9,15 +9,15 @@ namespace blackWildwBot
         private static ITelegramBotClient botClient;
         static void Main(string[] args)
         {
-            botClient = new TelegramBotClient("916400270:AAEEmmT9E52kdIDyKVw-sj0eGg-cHwEIWk4");
+            string KEY_TOKEN = Environment.GetEnvironmentVariable("token");
+            botClient = new TelegramBotClient(KEY_TOKEN);
             var me = botClient.GetMeAsync().Result;
             Console.WriteLine($"Bot id: {me.Id}. Bot name: {me.FirstName} ");
             botClient.OnMessage += Bot_OnMessege;
             botClient.StartReceiving();
-            Console.ReadKey();
+            Console.Read();
             botClient.StopReceiving();
         }
-       
         private static async void Bot_OnMessege(object sender, MessageEventArgs e)
         {
             var text = e?.Message.Text;
@@ -27,27 +27,28 @@ namespace blackWildwBot
 
             if (text == "/start")
             {
-                await botClient.SendTextMessageAsync(e.Message.Chat.Id, e.Message.Chat.Username +", Hey Pidor" + "\n" + "/help - A list of my commands").ConfigureAwait(false);
-                //await botClient.SendTextMessageAsync(e.Message.Chat.Id, "sadas").ConfigureAwait(false);
+                await botClient.SendTextMessageAsync(e.Message.Chat.Id, e.Message.Chat.Username + " Hey Pidor").ConfigureAwait(false);
                 return;
-
             }
-             if (text == "/Baton")
+            if (text == "/Baton")
             {
-                await botClient.SendPhotoAsync(e.Message.Chat.Id, "https://sun9-57.userapi.com/c625819/v625819495/20533/nkJHRbVW_Yo.jpg","I'am BIG BOSS").ConfigureAwait(false);
+                await botClient.SendPhotoAsync(e.Message.Chat.Id, "https://sun9-57.userapi.com/c625819/v625819495/20533/nkJHRbVW_Yo.jpg", "I'am BIG BOSS").ConfigureAwait(false);
                 return;
             }
             if (text == "/Yakuba")
             {
-               await botClient.SendAudioAsync(e.Message.Chat.Id, "https://github.com/Oh-Shit-God-Damn/tg-bot-blackWildw/blob/master/src/Yakuba.mp3");
+                await botClient.SendAudioAsync(e.Message.Chat.Id, "https://raw.githubusercontent.com/Oh-Shit-God-Damn/telegram-bot-blackWildw/master/blackWildwBot/music/Yakuba.mp3", "KING OF SPERMA");
+                return;
             }
-            if (text == "/help")
+            if (text == "/YakubaFapTime")
             {
-                await botClient.SendTextMessageAsync(e.Message.Chat,"/Baton" + "- calling Baton");
+                await botClient.SendAnimationAsync(e.Message.Chat.Id, "https://2gifs.ru/images/p78.media.tumblr.com/43fdb08744c97936a8e0576ed8f3a357/tumblr_omde5oZxvG1v2bt9eo1_500.gif");
+                return;
             }
             else
             {
-                await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: $"I don't know what is    <{text}> :(").ConfigureAwait(false);
+                await botClient.SendTextMessageAsync(e.Message.Chat.Id, text: $"I don't know what is    <{text}> :(").ConfigureAwait(false);
+                return;
             }
         }
     }
